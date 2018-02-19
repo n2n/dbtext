a52fdc2e9eaf559ba18e872c625caf8ed90428c9
nikolai-schmid <schmid@hnm.ch>
2018-02-19 11:27:03 +0100

    Add files via upload

---
+++ src/var/etc/dbtext/install.my.sql
@@ -0,0 +1,44 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server Version:               10.1.28-MariaDB - mariadb.org binary distribution
+-- Server Betriebssystem:        Win32
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Exportiere Struktur von Tabelle n2n_rocket_playground.dbtext_text_group
+CREATE TABLE IF NOT EXISTS `dbtext_text_group` (
+  `namespace` varchar(255) NOT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`namespace`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+-- Exportiere Struktur von Tabelle n2n_rocket_playground.dbtext_text_text
+CREATE TABLE IF NOT EXISTS `dbtext_text_text` (
+  `id` varchar(255) NOT NULL,
+  `group_namespace` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `dbtext_text_text_index_1` (`group_namespace`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+-- Exportiere Struktur von Tabelle n2n_rocket_playground.dbtext_text_text_t
+CREATE TABLE IF NOT EXISTS `dbtext_text_text_t` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `n2n_locale` varchar(12) DEFAULT NULL,
+  `str` varchar(255) DEFAULT NULL,
+  `text_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `dbtext_text_text_t_index_1` (`text_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
